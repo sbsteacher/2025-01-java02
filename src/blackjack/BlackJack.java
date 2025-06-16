24e774e408c7371191645e479203904971739bb0
sbsteacher <id@gmail.com>
2025-06-16 18:11:27 +0900

    DAY-15

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -1,5 +1,7 @@
 package blackjack;
 
+import java.util.List;
+
 public class BlackJack {
     public static void main(String[] args) {
         CardDeck cd = new CardDeck();
@@ -11,6 +13,29 @@ public class BlackJack {
 
         System.out.println("-----------------------");
         cd.printAllCards();
+        System.out.println("------- 게이머 카드 --------");
+        Gamer gamer = new Gamer();
+        gamer.receiveCard(c);
+        gamer.receiveCard(cd.getCard());
+        gamer.receiveCard(cd.getCard());
+        gamer.showCards();
+        List<Card> cards = gamer.openCards();
+        int score = Rule.getScore(cards); //A:1, 2~10:점수 그대로, J,Q,K: 10점
+        System.out.printf("score: %d\n", score);
+
+        System.out.println("------- 딜러 카드 --------");
+        Dealer dealer = new Dealer();
+        dealer.receiveCard(cd.getCard());
+        dealer.receiveCard(cd.getCard());
+        dealer.showCards();
+        System.out.printf("needMoreCard: %b\n", dealer.needMoreCard());
+        if(dealer.needMoreCard()) {
+            dealer.receiveCard(cd.getCard());
+        }
+        dealer.showCards();
+
+        Rule.getWinner(null, null);
+
     }
 }
 
